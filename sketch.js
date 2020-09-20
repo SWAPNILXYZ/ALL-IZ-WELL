38e424231c0f486eb2b0a4492866d62dd5871490
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-09-20 10:47:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
@@ -8,6 +8,11 @@ var score = 0;
 var turn = 0;
 var particle;
 var gameState = "start";
+
+var divisionHeight = 300;
+var plinkos = [];
+var particles = [];
+var divisions = [];
 
 function preload(){
 
@@ -38,85 +43,17 @@ for (var j = 50; j <= width - 10; j = j + 50){
 
 }
 
-var divisionHeight = 300;
-var plinkos = [];
-var particles = [];
-var divisions = [];
+
 
 function draw() {
   background(0); 
-  Engine.update(engine);
+  
   noStroke();
   
   textSize(20);
   fill("white");
   text("Score: "+score, 650, 20);
   
-for (var i = 0; i<plinkos.length; i++){
-  plinkos[i].display();
-}
-  for (var j = 0; j < particles.length; j++){
-    particles[j].display();
-  }
-  for (var k = 0; k < divisions.length; k++){
-    divisions[k].display();
-  }
-
-  ground.display();
- 
-  
-  if (particle!=null){
-    particle.display();
-
-    if (particle.body.position.y > 760){
-
-      if (particle.body.position.x < 300){
-        score = score + 500;
-        turn++; 
-        particle = null;
-       
-      }
-    }
-  }
-  if (particle!=null){
-    particle.display();
-
-    if (particle.body.position.y > 760){
-
-      if (particle.body.position.x > 301 && particle.body.position.x < 600){
-        score = score + 100;
-        turn++; 
-        particle = null;
-       
-      }
-    }
-  }
-  if (particle!=null){
-    particle.display();
-
-    if (particle.body.position.y > 760){
-
-      if (particle.body.position.x > 601 && particle.body.position.x < 900){
-        score = score + 200;
-        turn++; 
-        particle = null;
-        
-      }
-    }
-  }
-  if (gameState !== "end"){
-    text("place your mouse pointer to position the particle. Click space to make it fall", 50, 450);
-  }
-  if (turn >= 5){
-    gameState = "end";
-
-    if (gameState === "end"){
-      noStroke();
-      textSize(50);
-      fill("white");
-      text("GameOver", 300, 450);
-    }
-  }
   textSize(25);
   text("500", 20, 600);
   text("500", 100, 600);
@@ -125,16 +62,68 @@ for (var i = 0; i<plinkos.length; i++){
   text("100", 340, 600);
   text("100", 420, 600);
   text("100", 500, 600);
-  text("100", 580, 600);
+  text("200", 580, 600);
   text("200", 660, 600);
   text("200", 740, 600);
+  Engine.update(engine);
+  ground.display();
+
+  if (gameState === "end"){
+    noStroke();
+    textSize(50);
+    fill("white");
+    text("GameOver", 300, 450);
+  }
+
+for (var i = 0; i < plinkos.length; i++){
+  plinkos[i].display();
+}
+    if(particle!=null)
+    {
+       particle.display();
+        
+        if (particle.body.position.y>760)
+        {
+              if (particle.body.position.x < 300) 
+              {
+                  score=score+500;      
+                  particle=null;
+                  if ( turn>= 5) gameState ="end";                          
+              }
+
+
+              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
+              {
+                    score = score + 100;
+                    particle=null;
+                    if ( turn>= 5) gameState ="end";
+
+              }
+              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
+              {
+                    score = score + 200;
+                    particle=null;
+                    if ( turn>= 5)  gameState ="end";
+
+              }      
+              
+        }
+  
+      }
+  //if (gameState !== "end"){
+  //  text("place your mouse pointer to position the particle. Click space to make it fall", 50, 450);
+  //}
+  for (var k = 0; k < divisions.length; k++){
+    divisions[k].display();
+  }
+
   drawSprites();
 }
+function mouseClicked(){
+  if (gameState !== "end"){
 
-function keyPressed(){
-  if (keyCode === 32 && gameState !== "end"){
-
-  
+  turn++;
     particle = new Particles(mouseX, 10, 10, 10);
   }
 }
+
